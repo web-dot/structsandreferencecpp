c63180a653825a6b8f47178b8eaa915e6ac4949e
web-dot <fedev.sudipto@gmail.com>
2022-10-22 16:19:47 +0530

    c1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+        C++ Structures:
+        Structures also called structs are a way to group several related variables into
+        one place. Each variable in a struct is called a "member" of the struct.
+
+    */
+}
